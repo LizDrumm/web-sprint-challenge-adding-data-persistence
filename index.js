e7fb67b78b1f4e9a650a18b3dec0abae8b4752ce
Elizabeth Drumm <elizabethdrumm@Elizabeths-MacBook-Pro.local>
2020-11-06 13:48:05 -0600

    moved dynamic route

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const PORT = process.env.PORT || 4001;
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
-});
+}); 
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -3,26 +3,14 @@ const express = require('express');
 const Projects = require('./projects-model');
 
 const router = express.Router();
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { 
     Projects.getProjects()
     .then(projs => {
-        res.status(200).json({ data: projs })
+        res.status(200).json({ data: projs }) 
     })
     .catch(err => {
         res.status(500).json({ message: 'Failed to get projects'});
-    });
-});
-
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    Projects.getProjectsById(id)  
-    .then(projs => {
-        res.status(200).json({ data: projs })
-    })
-    .catch(error => { 
-        res.status(500).json({ message: 'Failed to get projects', error:error.message});
-    });
+    }); 
 });
 
 
@@ -71,7 +59,16 @@ router.get('/tasks/:id', (req, res) => {
     });
 });
 
-
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Projects.getProjectsById(id)  
+    .then(projs => {
+        res.status(200).json({ data: projs })
+    })
+    .catch(error => { 
+        res.status(500).json({ message: 'Failed to get projects', error:error.message});
+    });
+});
 
 router.post('/', (req, res) => {
     const projectData = req.body;
